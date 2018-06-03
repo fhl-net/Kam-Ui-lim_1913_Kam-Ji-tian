58235e703ab1942f43336ebbb6fb8993734fe235
薛丞宏 <ihcaoe@gmail.com>
2018-06-03 08:45:17 +0800

    2018/05/23 e csv chiau id pai, i-au chiah-ho khuann cha-piat

--- sql2csv.sh
+++ sql2csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The̍h-tiāu ip
-echo "select id,word,other,chinese,exp,example,username,modtime,han,english,page from dic_can;" | \
+echo "SELECT id,word,other,chinese,exp,example,username,modtime,han,english,page FROM dic_can ORDER BY id;" | \
   cat $1 - | \
   sed '/CREATE INDEX work_dic_can ON dic_can USING btree (word);/d' | \
   sed '/ALTER TABLE/{N;d;}' | \
